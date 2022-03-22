7fe3e068a19b7dd3a9b8205bc8e881ee974ce13a
MaradiSu <p.suresh@campusuvce.in>
2022-03-22 20:52:27 +0530

    sample

--- 01-Basics-Understanding-Terms/main.tf
+++ 01-Basics-Understanding-Terms/main.tf
@@ -7,15 +7,15 @@
 
 # //////////////////////////////
 # PROVIDERS
-# //////////////////////////////
 terraform {
-    required_providers {
-        aws = {
-            source  = "hashicorp/aws"
-            version = "~> 1.0.4"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
     }
+  }
 }
+
 provider "aws" {
   access_key = ""
   secret_key = ""
