93c3bbb12efe176f2de1eb3256189fa2c3b470ef
MaradiSu <p.suresh@campusuvce.in>
2022-03-22 20:04:15 +0530

    sample

--- 01-Basics-Understanding-Terms/main.tf
+++ 01-Basics-Understanding-Terms/main.tf
@@ -1,7 +1,6 @@
 # //////////////////////////////
 # VARIABLES
 # //////////////////////////////
-
 # //////////////////////////////
 # Terraform Configuration
 # //////////////////////////////
@@ -9,12 +8,37 @@
 # //////////////////////////////
 # PROVIDERS
 # //////////////////////////////
-
+terraform {
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~> 1.0.4"
+        }
+    }
+}
+provider "aws" {
+  access_key = ""
+  secret_key = ""
+  region     = ""
+}
 
 # //////////////////////////////
 # RESOURCES
 # //////////////////////////////
-# S3 Bucket
+
+# S3 Bucket 
+resource "aws_s3_bucket" "my_s3_bucket" {
+    bucket = "my-s3-bucket-maradi-01"
+    versioning {
+        enabled = true
+    }
+}
+
+#IAM User
+
+resource "aws_iam_user" "my_iam_user" {
+    name = "Terraform-Practice"
+}
 
 # //////////////////////////////
 # OUTPUT
